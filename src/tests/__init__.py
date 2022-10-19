67663d2aff504756d6ea2dcbb26f8447a32200c2
hallowslab <hallow.tech.lab@gmail.com>
2022-10-19 18:17:11 +0100

    Updated tests

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -9,9 +9,17 @@ VALID_HOSTS = {
 INVALID_HOSTS = ["cpan", "plesk40000000312", "webxl123", "iberweb4401", "random_host"]
 
 RANDOM_VALID_CREDS = [
-    "random_mail@gmail.com kpO8swKLR0rKN4LKIbPV random_mail@gmail.com kpO8swKLR0rKN4LKIbPV",
-    "not_random_mail@gmail.com J1vDjXNiec8huygCItpF not_random_mail@gmail.com J1vDjXNiec8huygCItpF",
-    "random_mail@yahoo.com ZASmGQqHo4iDgjWaN-rP random_mail@yahoo.com ZASmGQqHo4iDgjWaN-rP",
-    "random_mail@gmail.com 7c6EtdZYlfLaCz9JNy1s random_mail@gmail.com 7c6EtdZYlfLaCz9JNy1s",
-    "random_mail@gmail.com 2GthJDlT_KeFCzZ60Vo8 random_mail@gmail.com 2GthJDlT_KeFCzZ60Vo8",
+    "random_mail1@gmail.com kpO8swKLR0rKN4LKIbPV random_mail11@gmail.com kpO8swKLR0rKN4LKIbPV",
+    "not_random_mail1@gmail.com J1vDjXNiec8huygCItpF not_random_mail11@gmail.com J1vDjXNiec8huygCItpF",
+    "random_mail2@yahoo.com ZASmGQqHo4iDgjWaN-rP random_mail22@yahoo.com ZASmGQqHo4iDgjWaN-rP",
+    "random_mail3@gmail.com 7c6EtdZYlfLaCz9JNy1s random_mail33@gmail.com 7c6EtdZYlfLaCz9JNy1s",
+    "random_mail4@gmail.com 2GthJDlT_KeFCzZ60Vo8 random_mail44@gmail.com 2GthJDlT_KeFCzZ60Vo8",
+]
+
+RANDOM_VALID_CREDS_2 = [
+    "random_mail@gmail.com kpO8swKLR0rKN4LKIbPV",
+    "not_random_mail@gmail.com J1vDjXNiec8huygCItpF",
+    "random_mail@yahoo.com ZASmGQqHo4iDgjWaN-rP",
+    "random_mail@gmail.com 7c6EtdZYlfLaCz9JNy1s",
+    "random_mail@gmail.com 2GthJDlT_KeFCzZ60Vo8",
 ]
