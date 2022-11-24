99fcfe8ee31f6b067777a4ca109734dd1edea9a1
hallowslab <hallow.tech.lab@gmail.com>
2022-11-24 16:30:50 +0000

    Working on login integration, added flask praetorian for api security, and flask migrate for database upgrades, updated templates, code clean

--- src/manage.py
+++ src/manage.py
@@ -1,10 +1,32 @@
+import click
 from flask.cli import FlaskGroup
 
-from server import create_flask_app
+from server import create_flask_app, guard
+from server.models import users, db
+from flask.cli import with_appcontext
 
 
 app = create_flask_app()
 cli = FlaskGroup(create_app=create_flask_app)
+
+
+@app.cli.command("create-user")
+@click.argument("user")
+def create_user(user):
+    new_user = users.User(
+        user, "pymap@localhost", guard.hash_password("CH4NG3_M$"), "admin"
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    ctx_str = f"\nCreated User:\nUser: {user}\nEmail: {'pymap@localhost'}\nPassword: {'CH4NG3_M$'}\nRoles: {'admin'}======="
+    print(ctx_str)
+
+
+@app.cli.command("create-db")
+@with_appcontext
+def create_db():
+    db.create_all()
+    db.session.commit()
 
 
 if __name__ == "__main__":
