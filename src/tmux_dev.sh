341adaa20bc816b30252234539e5e6a123e90a87
hallowslab <hallow.tech.lab@gmail.com>
2024-03-08 23:40:07 +0000

    project files

--- src/tmux_dev.sh
+++ src/tmux_dev.sh
@@ -6,18 +6,18 @@ tmux new-session -ds $session
 
 window=0
 
-tmux rename-window -t $session:$window "Django"
-tmux send-keys -t $session:$window "redis-server" C-m
+tmux rename-window -t "$session:$window" "Django"
+tmux send-keys -t $session:$window "sleep 5 && poetry run task serverDev" C-m
 
-tmux select-window -t $session:$window
+tmux select-window -t "$session:$window"
 
 tmux split-window -h
-tmux send-keys "poetry run task serverDev" C-m
+tmux send-keys "redis-server" C-m
 
-tmux split-window -v -t $session:$window.$window
+tmux split-window -v -t "$session:$window.$window"
 tmux send-keys "poetry run task workerDev" C-m
 
-tmux split-window -v -t $session:$window.2
-tmux send-keys "poetry run task monitorDev" C-m
+tmux split-window -v -t "$session:$window.2"
+tmux send-keys "sleep 5 && poetry run task monitorDev" C-m
 
-tmux attach -t $session
+tmux attach -t "$session"
