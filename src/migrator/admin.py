d795f3cb8db0118773e7563a7a524b9985acdafe
hallowslab <hallow.tech.lab@gmail.com>
2024-05-18 15:20:02 +0100

    Admin function to archive tasks

--- src/migrator/admin.py
+++ src/migrator/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.utils.translation import ngettext
 
 from .models import CeleryTask
 
 # Register your models here.
 
-admin.site.register(CeleryTask)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["task_id","source","destination","owner", "start_time"]
+    ordering = ["-start_time"]
+    actions = ["archive_selected"]
+
+    @admin.action(description="Mark selected tasks as archived")
+    def archive_selected(self, request, queryset):
+        queryset.update(archived=True)
+        self.message_user(
+            request,
+            ngettext(
+                "%d task was successfully marked as archived.",
+                "%d Tasks were successfully marked as archived.",
+                updated,
+            )
+            % updated,
+            messages.SUCCESS,
+        )
+
+admin.site.register(CeleryTask, TaskAdmin)
