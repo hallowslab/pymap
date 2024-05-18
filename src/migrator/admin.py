51b3795c80e10ebe35be1162f7ea827723544663
hallowslab <hallow.tech.lab@gmail.com>
2024-05-18 21:30:35 +0100

    Update admin action for archiving tasks

--- src/migrator/admin.py
+++ src/migrator/admin.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
-from django.contrib import messages
+from django.contrib import admin, messages
+from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import ngettext
+
+from django_celery_results.models import TaskResult
 
 from .models import CeleryTask
 
@@ -12,9 +14,32 @@ class TaskAdmin(admin.ModelAdmin):
     ordering = ["-start_time"]
     actions = ["archive_selected"]
 
-    @admin.action(description="Mark selected tasks as archived")
+    @admin.action(description="Archive selected tasks")
     def archive_selected(self, request, queryset):
-        queryset.update(archived=True)
+        for task in queryset:
+            try:
+                result = TaskResult.objects.filter(task_id=task.task_id)
+                if result.exists():
+                    result.delete()
+            except ImproperlyConfigured:
+                # If TaskResult model is not available, log or handle the situation
+                self.message_user(
+                    request,
+                    "Task results backend is not configured to use django-db.",
+                    messages.WARNING,
+                )
+                break  # Exit the loop if the backend is not configured
+            except Exception as e:
+                # Handle other exceptions
+                self.message_user(
+                    request,
+                    f"An error occurred while deleting task results: {e}",
+                    messages.ERROR,
+                )
+                continue  # Continue to the next task
+
+        # Update the queryset to mark tasks as archived
+        updated = queryset.update(archived=True)
         self.message_user(
             request,
             ngettext(
