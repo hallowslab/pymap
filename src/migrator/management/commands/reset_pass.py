22c792bd9a7de77712c7d727c65cc5d712d3bbab
hallowslab <hallow.tech.lab@gmail.com>
2024-05-23 18:55:11 +0100

    Command to reset user password, might need to be updated to actually raise CommandError when certain checks are not met if ever used programatically (ex:checking exit code)

---
+++ src/migrator/management/commands/reset_pass.py
@@ -0,0 +1,50 @@
+from typing import Any, List
+from django.core.management.base import BaseCommand, CommandParser
+from django.contrib.auth.models import User, AbstractBaseUser
+from django.utils.crypto import get_random_string
+
+
+class Command(BaseCommand):
+    help = "Resets the password for the supplied user, to a randomly generated one"
+    DOMAINS: List[str] = [
+        "example.com",
+        "example.tld",
+        "pymap.com",
+        "pymap.io",
+        "pymap.lan",
+    ]
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument("username", type=str, help="Username of the account")
+        parser.add_argument(
+            "--length",
+            type=int,
+            default=20,
+            choices=range(15, 21),
+            help="Length of the new password to be generated",
+        )
+        return super().add_arguments(parser)
+
+    def handle(self, *args: object, **options: Any) -> None:
+        username = options["username"]
+        length = options["length"]
+        user = User.objects.filter(username=username)
+        assert isinstance(user, AbstractBaseUser)
+        if not user.exists():
+            self.stderr.write(self.style.ERROR(f"User {username} does not exist"))
+            return
+        _new_password = get_random_string(length)
+        user.set_password(_new_password)
+        if not user.check_password(_new_password):
+            self.stderr.write(
+                self.style.ERROR(
+                    f"We were unable to set the password to {_new_password}"
+                )
+            )
+            return
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Password for user {username} has been updated to {_new_password}"
+            )
+        )
+        user.save()
