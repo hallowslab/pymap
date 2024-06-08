288a36df1ec89fb74264eaf42de468635b23ae77
hallowslab <hallow.tech.lab@gmail.com>
2024-06-08 13:29:59 +0100

    Admin <Archive task> action now warns for each result failed to delete, searches for async results and tries to forget stored results

--- src/migrator/admin.py
+++ src/migrator/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin, messages
-from django.core.exceptions import ImproperlyConfigured
+from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
 from django.utils.translation import ngettext
+from celery.result import AsyncResult
 
 from django_celery_results.models import TaskResult
 
 from .models import CeleryTask
+from pymap import celery_app
 
 # Register your models here.
 
@@ -18,9 +20,21 @@ class TaskAdmin(admin.ModelAdmin):
     def archive_selected(self, request, queryset) -> None:
         for task in queryset:
             try:
-                result = TaskResult.objects.filter(task_id=task.task_id)
-                if result.exists():
-                    result.delete()
+                result = AsyncResult(task.task_id, app=celery_app)
+                result.get(timeout=5.0)
+                result.forget()
+            except TimeoutError:
+                self.message_user.error(request, f"Failed to clear results for Task ID: {task.task_id}", messages.WARNING)
+            try:
+                result = TaskResult.objects.get(task_id=task.task_id)
+                result.delete()
+            except ObjectDoesNotExist:
+                self.message_user(
+                    request,
+                    f"The result does not seem to exist in our database: {task.task_id}",
+                    messages.WARNING,
+                )
+                continue  # Continue to the next task
             except ImproperlyConfigured:
                 # If TaskResult model is not available, log or handle the situation
                 self.message_user(
