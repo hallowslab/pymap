a2d3daccbe00ff9b0f50d9b237702583983c6c68
hallowslab <hallow.tech.lab@gmail.com>
2024-06-30 23:56:00 +0100

    better default for static root

--- src/pymap/settings.py
+++ src/pymap/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "/home/pymap/app/static"
+STATIC_ROOT = os.environ.get("STATIC_ROOT", Path(BASE_DIR, "static"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
