852c5e55429b1b4d3180cc81a31fe91e7467b6c9
hallowslab <hallow.tech.lab@gmail.com>
2024-08-19 00:19:57 +0100

    adds retry task endpoint

--- src/migrator/views.py
+++ src/migrator/views.py
@@ -10,7 +10,13 @@ from django.shortcuts import redirect, render
 from django.conf import settings
 from django.urls import reverse
 from django.utils.functional import SimpleLazyObject
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect, HttpRequest, HttpResponseServerError
+from django.http import (
+    HttpResponse,
+    JsonResponse,
+    HttpResponseRedirect,
+    HttpRequest,
+    HttpResponseServerError,
+)
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db.models import Sum, Q
@@ -163,7 +169,7 @@ def sync(request: HttpRequest) -> (HttpResponse | HttpResponseRedirect):
             #     "Received the following output from generator:\n %s", content
             # )
 
-            task = call_system.apply_async((content,), countdown=5)
+            task = call_system.apply_async((content,), countdown=5)  # type: ignore
             logger.info(
                 f"Starting background task with ID: {task.id} from User: {user.username}"
             )
@@ -195,7 +201,9 @@ def sync(request: HttpRequest) -> (HttpResponse | HttpResponseRedirect):
 
 
 @login_required
-def retry_task(request:HttpRequest, task_id:str)-> (HttpResponse|HttpResponseRedirect):
+def retry_task(
+    request: HttpRequest, task_id: str
+) -> (HttpResponse | HttpResponseRedirect):
     assert isinstance(request.user, User)
     user = request.user
     source = None
@@ -204,27 +212,20 @@ def retry_task(request:HttpRequest, task_id:str)-> (HttpResponse|HttpResponseRed
         db_result = CeleryTask.objects.get(task_id=task_id)
         source = db_result.source
         destination = db_result.destination
-    except CeleryTask.DoesNotExist:
-        logger.info(f"Missing database result for {task_id}")
-        pass
-    try:
-        meta=celery_app.backend.get_task_meta(task_id)
-        app_task = celery_app.tasks[meta['name']]
-        content = meta['args']
-        task = app_task.apply_async((content,), countdown=5)
+        meta = celery_app.backend.get_task_meta(task_id)
+        content = meta["args"][0]
+        logger.debug(f"Content: {content}")
+        task = call_system.apply_async((content,), countdown=5)  # type: ignore
         logger.info(
-            f"Starting background task with ID: {task.id} from User: {user.username}"
+            f"Restarting task {task_id} in background with new ID: {task.id} from User: {user.username}"
         )
-        logger.debug(meta)
-        return HttpResponseServerError("Not implemented")
-        source = meta[""]
-        destination = meta[""]
         # Don't forget to save the task id and respective inputs to the database
         root_log_directory: str = settings.PYMAP_LOGDIR
         log_directory = Path(root_log_directory, task.id)
         if not log_directory.exists():
             log_directory.mkdir()
-        domains = ", ".join(db_result.domains)
+        db_domains = db_result.domains if db_result.domains is not None else []
+        domains = ", ".join(db_domains)
         ctask = CeleryTask(
             task_id=task.id,
             source=source,
@@ -234,14 +235,19 @@ def retry_task(request:HttpRequest, task_id:str)-> (HttpResponse|HttpResponseRed
             domains=domains,
             owner=user,
         )
-        #ctask.save()
+        ctask.save()
 
         target_url = reverse("migrator:tasks-details", args=(task.id,))
         logger.debug("Target redirect: %s", target_url)
         return redirect(target_url, permanent=False)
+    except CeleryTask.DoesNotExist:
+        return HttpResponseServerError(
+            "This task no longer has results on the database"
+        )
     except Exception as e:
         logger.critical("Unhandled exception: %s", str(e), exc_info=True)
         return HttpResponseServerError("Internal error")
+
 
 @login_required
 def download_log(request: HttpRequest, task_id: str, filename: str) -> HttpResponse:
