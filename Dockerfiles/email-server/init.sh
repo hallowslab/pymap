bdd58c878f99202ca5ece994ad91c2cb1024a303
hallowslab <hallow.tech.lab@gmail.com>
2024-09-08 03:10:40 +0100

    mail server

--- Dockerfiles/email-server/init.sh
+++ Dockerfiles/email-server/init.sh
@@ -1,14 +1,4 @@
-mkdir -p /var/mail/pymap/Maildir/{cur,new,tmp}
-chown -R pymap:pymap /var/mail/pymap
-echo 'pymap:Password123' > /etc/dovecot/users
-
-
-mkdir -p /var/mail/test/Maildir/{cur,new,tmp}
-chown -R test:test /var/mail/test
-echo 'test:Password123' >> /etc/dovecot/users
-
-
 service dovecot start
 service postfix start
 
-tail -f /var/log/postfix.log /var/log/dovecot.log
+tail -f /var/log/dovecot.log /var/log/postfix.log /var/log/mail.log
