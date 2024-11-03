816e5b4f479fdd4db5170f17f37711fa0abe9a92
hallowslab <hallow.tech.lab@gmail.com>
2024-11-03 19:55:22 +0000

    messages are now generated from an imported dataset

--- Dockerfiles/email-server/generate_messages.sh
+++ Dockerfiles/email-server/generate_messages.sh
@@ -3,29 +3,16 @@
 # Configuration
 sender="pymap@mail.pymap.lan"
 recipients=("pymap@mail.pymap.lan" "test@mail.pymap.lan")
-num_messages=5000
-subjects_file="subjects.txt"
-bodies_file="bodies.txt"
+dataset_path="/var/lib/spamassassin-dataset"  # Path to your email files
 
-# Load random subjects and bodies from external files
-generate_random_subject() {
-  shuf -n 1 "$subjects_file"
-}
-
-generate_random_body() {
-  shuf -n 1 "$bodies_file"
-}
-
-# Loop through recipients and send multiple emails
 for recipient in "${recipients[@]}"; do
-  for ((i = 1; i <= num_messages; i++)); do
-    subject=$(generate_random_subject)
-    body=$(generate_random_body)
-
-    # Send the email
-    echo "Generating message from $sender to $recipient" >> generate.txt
-    echo "$body" | mail -s "$subject" -r "$sender" "$recipient"
-    
-    echo "Sent email #$i to $recipient with subject '$subject'"
+  # Loop through email files in dataset directory
+  for email_file in "$dataset_path"/*; do
+    # Ensure we only process regular files
+    if [[ -f "$email_file" ]]; then
+      # Inject the email to the specified recipient using Postfix's sendmail
+      sendmail -f "$sender" "$recipient" < "$email_file"
+      echo "Injected $(basename "$email_file") to $recipient"
+    fi
   done
 done
--- Dockerfiles/email-server/init.sh
+++ Dockerfiles/email-server/init.sh
@@ -1,9 +1,27 @@
 service dovecot start
 service postfix start
 
+start_dir=$PWD
+
 # Check if GENERATE_MESSAGES environment variable is set to 'true'
 if [ "$GENERATE_MESSAGES" = "true" ]; then
+    # Get the apache spamassassin ham dataset from https://spamassassin.apache.org/old/publiccorpus/
+    echo "Fetching samples from spamassassin dataset";
+    mkdir -p /var/lib/spamassassin-dataset;
+    cd /var/lib/spamassassin-dataset;
+    sleep 2;
+    curl -o easy_ham.tar.bz2 https://spamassassin.apache.org/old/publiccorpus/20021010_easy_ham.tar.bz2;
+    curl -o easy_ham2.tar.bz2 https://spamassassin.apache.org/old/publiccorpus/20030228_easy_ham.tar.bz2;
+    curl -o hard_ham.tar.bz2 https://spamassassin.apache.org/old/publiccorpus/20021010_hard_ham.tar.bz2;
+    curl -o hard_ham2.tar.bz2 https://spamassassin.apache.org/old/publiccorpus/20030228_hard_ham.tar.bz2;
+    tar -xvjf easy_ham.tar.bz2 --strip-components=1;
+    tar -xvjf easy_ham2.tar.bz2 --strip-components=1;
+    tar -xvjf hard_ham.tar.bz2 --strip-components=1;
+    tar -xvjf hard_ham2.tar.bz2 --strip-components=1;
+    rm -f easy_ham.tar.bz2 easy_ham2.tar.bz2 hard_ham.tar.bz2 hard_ham2.tar.bz2;
+    cd $start_dir;
     # Call the script to generate and send messages
+    echo "Generating Messages";
     ./generate_messages.sh
 fi
 
